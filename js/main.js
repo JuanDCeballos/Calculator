2c427e89fd94355b3e61ac271ef634f6d8d58495
Juan Ceballos <juandavi789@hotmail.com>
2021-08-12 20:24:15 -0500

    trying to figuring out how to store the numbers

--- js/main.js
+++ js/main.js
@@ -1,59 +1,45 @@
 const buttons = document.querySelectorAll('.cta');
 const ac = document.querySelector('.ac');
+const equal = document.querySelector('.equal');
 const numberButtons = document.querySelectorAll('.number');
 const operandButtons = document.querySelectorAll('.operand');
 const displayOperation = document.getElementById('operation');
 const displayResult = document.getElementById('result');
 
-let num1 = [];
-let num2 = [];
-// let num1ToInt;
-let operand = [];
+let num1 = '';
+let num2 = '';
+let operand = '';
 
 ac.addEventListener('click', () => {
   displayOperation.textContent = '';
   displayResult.textContent = '';
-  num1 = [];
-  num2 = [];
-  operand = [];
+  num1 = '';
+  num2 = '';
+  operand = undefined;
+});
+
+equal.addEventListener('click', () => {
+  displayResult.textContent = operate(Number(num1), Number(num2), operand);
 });
 
 numberButtons.forEach((numberButton) => {
   numberButton.addEventListener('click', () => {
-    if (
-      operand.includes('+') ||
-      operand.includes('*') ||
-      operand.includes('-') ||
-      operand.includes('/')
-    ) {
-      num2.push(numberButton.id);
+    if (!operand) {
+      num1 += numberButton.id;
+      populateDisplay(numberButton.id);
     } else {
-      num1.push(numberButton.id);
+      num2 += numberButton.id;
+      populateDisplay(numberButton.id);
     }
-    populateDisplay(numberButton.id);
   });
 });
 
 operandButtons.forEach((operandButton) => {
   operandButton.addEventListener('click', () => {
-    let op = operandButton.id;
-    operand.push(op);
-    populateDisplay(op);
-    if (operandButton.id === '=') {
-      let num1ToEvaluate = lmao(num1);
-      let num2ToEvaluate = lmao(num2);
-      displayResult.textContent = operate(
-        num1ToEvaluate,
-        num2ToEvaluate,
-        operand[0]
-      );
-    }
+    operand = operandButton.id;
+    populateDisplay(operand);
   });
 });
-
-function lmao(arr) {
-  return Number(arr.join(''));
-}
 
 function populateDisplay([...theArgs]) {
   displayOperation.textContent += theArgs;
